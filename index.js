943ef159f43000bd7e1d7ac5ae96f6cf45290e7e
Guilherme Luís <guilhermeffable@gmail.com>
2021-02-18 17:55:24 +0000

    Added prefix customizabilty

--- index.js
+++ index.js
@@ -38,33 +38,114 @@ client.on("ready", ()  => {
 
 client.on("message", async (message) => {
 
-    let dispatcher;
-   
-    const prefix = "!";
+    const {prefix} = require('./json/config.json');
 
-    const serverQueue = queue.get(message.guild.id);
+    let prefixes = JSON.parse(fs.readFileSync("./json/prefixes.json", "utf-8"));
+
+    if(!prefixes[message.guild.id]){
+
+        prefixes[message.guild.id] = {
+
+            prefixes: prefix,
+
+        };
+
+    }
+
+    let prefixNew = prefixes[message.guild.id].prefixes;
+
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
 
     const command = args.shift().toLowerCase();
 
-    if(!message.content.startsWith(prefix) || message.author.bot) return;
+    if(!message.content.startsWith(prefixNew) || message.author.bot) return;
 
     const cmd = client.commands.get(command) || client.commands.get(client.aliases.get(command));
 
     if(!cmd) return;
-
     try{
-        
-        cmd.run(client, message, args, queue, searcher, prefix);
+        cmd.run(client, message, args, queue, searcher, prefixNew);
     }catch(err){
         return console.error(err);
     }
 
-    
-
 
 });
 
+
+
+
+let skip = (message, serverQueue) => {
+
+    
+    if(!message.member.voice.channel){
+
+        return message.channel.send("Tens de estar conectado/a a um voice channel.");
+
+    }
+
+    if(!serverQueue) {
+        
+        return message.channel.send("Não há nada para reproduzir!");
+        
+    }
+
+    serverQueue.connection.dispatcher.end();
+};
+
+let showQueue = (client, message, serverQueue) => {
+
+    if(!message.member.voice.channel){
+
+        return message.channel.send("Tens de estar conectado/a a um voice channel.");
+
+    }
+
+    if(!serverQueue) {
+        
+        return message.channel.send({embed:{
+            color: 3447003,
+            author:{
+                name: client.user.username,
+            icon_url: client.user.avatarURL()
+        },
+            title: "Queue",
+            description: "Músicas na Queue",
+            fields:[{
+                name:"Nada para reproduzir.",
+                value: "0"
+            },
+            ]
+        }});
+        
+    }
+    else{
+
+        let embed = {
+            color: 3447003,
+            author:{
+                name: client.user.username,
+            icon_url: client.user.avatarURL()
+        },
+            title: "Queue",
+            description: "Músicas na Queue",
+            fields:[]
+        
+        };
+
+        serverQueue.songs.map( (item, pos) => {
+
+            embed.fields.push({
+                name: `${pos + 1} - ${item.title}`});
+
+        })
+
+
+        return message.channel.send({embed});
+
+    }
+
+}
 
 
 

9b859c109b70cbce40bb87ebb7f63fd5e95b5129
guilherme.luis <guilherme.luis@janeladigital.com>
2022-04-06 12:34:36 +0100

    ongoing - ytsearch installed and code cleanup

--- commands/play.js
+++ commands/play.js
@@ -3,137 +3,132 @@ const ytpl = require('ytpl');
 const Discord = require('discord.js');
 
 module.exports.run = async (client, message, args, queue, searcher) => {
+	const vc = message.member.voice.channel;
 
-    const vc = message.member.voice.channel;
+	if (!vc)
+		return message.channel.send('Tens de estar conectado a um canal de voz!');
 
-    if(!vc) return message.channel.send("Tens de estar conectado a um canal de voz!");
+	let url = args.join(' ');
 
-    let url = args.join(" ");
+	let play = (guild, song) => {
+		const serverQueue = queue.get(guild.id);
 
-    
-    let play = (guild, song) => {
+		if (!song) {
+			serverQueue.vChannel.leave();
+			queue.delete(guild.id);
+			return;
+		}
 
-        const serverQueue = queue.get(guild.id);
+		const dispatcher = serverQueue.connection
+			.play(ytdl(song.url))
+			.on('finish', () => {
+				serverQueue.songs.shift();
+				play(guild, serverQueue.songs[0]);
+			});
 
-        if(!song){
+		let seconds =
+			serverQueue.songs[0].vLength -
+			60 * parseInt(serverQueue.songs[0].vLength / 60);
 
-            serverQueue.vChannel.leave();
-            queue.delete(guild.id);
-            return;
+		let dur = `${parseInt(serverQueue.songs[0].vLength / 60)}:${
+			seconds < 10 ? '0' + seconds : seconds
+		}`;
 
-        }
+		let msg = new Discord.MessageEmbed()
+			.setTitle('A tocar: ')
+			.setDescription(
+				`[${serverQueue.songs[0].title}](${serverQueue.songs[0].url})`,
+				'_______'
+			)
+			.addField('Duração', dur, true)
+			.addField('\u200B', '\u200B', true)
+			.addField('Pedida por:', `${serverQueue.songs[0].askedBy}`)
+			.setImage(serverQueue.songs[0].thumbnail)
+			.setColor('DARK_VIVID_PINK')
+			.setTimestamp(new Date())
+			.setFooter(message.guild.name, message.guild.iconURL());
 
-        const dispatcher = serverQueue.connection
-            .play(ytdl(song.url))
-            .on('finish', () => {
-                serverQueue.songs.shift();
-                play(guild, serverQueue.songs[0]);
-            })
-            
-        let seconds = serverQueue.songs[0].vLength - 60 * parseInt(serverQueue.songs[0].vLength / 60);
-         
-        let dur = `${parseInt(serverQueue.songs[0].vLength / 60)}:${seconds < 10 ? '0' + seconds : seconds}`;
-        let msg = new Discord.MessageEmbed()
-            .setTitle("A tocar: ")
-            .setDescription(`[${serverQueue.songs[0].title}](${serverQueue.songs[0].url})`, "_______")
-            .addField("Duração", dur, true)
-            .addField('\u200B', '\u200B', true)
-            .addField("Pedida por:", `${serverQueue.songs[0].askedBy}`)
-            .setImage(serverQueue.songs[0].thumbnail)
-            .setColor("DARK_VIVID_PINK")
-            .setTimestamp(new Date())
-            .setFooter(message.guild.name, message.guild.iconURL())
-        
-        serverQueue.txtChannel.send(msg);
-        
+		serverQueue.txtChannel.send(msg);
+	};
 
+	let videoHandler = async (songInfo, message, vc, playlist = false) => {
+		const serverQueue = queue.get(message.guild.id);
 
-    }
+		const song = {
+			title: songInfo.videoDetails.title,
+			url: songInfo.videoDetails.video_url,
+			vLength: songInfo.videoDetails.lengthSeconds,
+			thumbnail: songInfo.videoDetails.thumbnails[3].url,
+			askedBy: message.author.toString(),
+			askedByUsername: message.author.username,
+		};
 
-    let videoHandler = async (songInfo, message, vc, playlist = false) => {
+		if (!serverQueue) {
+			const queueConstructor = {
+				txtChannel: message.channel,
+				vChannel: vc,
+				connection: null,
+				songs: [],
+				volume: 10,
+				playing: true,
+			};
 
-        const serverQueue = queue.get(message.guild.id);
+			queue.set(message.guild.id, queueConstructor);
 
-        const song = {
+			queueConstructor.songs.push(song);
 
-            title: songInfo.videoDetails.title,
-            url: songInfo.videoDetails.video_url,
-            vLength: songInfo.videoDetails.lengthSeconds,
-            thumbnail: songInfo.videoDetails.thumbnails[3].url,
-            askedBy: message.author.toString(),
-            askedByUsername: message.author.username
+			try {
+				let connection = await vc.join();
+				message.guild.me.voice.setSelfDeaf(true);
+				queueConstructor.connection = connection;
+				play(message.guild, queueConstructor.songs[0]);
+			} catch (err) {
+				console.error(err);
+				queue.delete(message.guild.id);
+				return message.channel.send(
+					`Não foi possível conectar-me ao voice channel ${err}.`
+				);
+			}
+		} else {
+			let msg = new Discord.MessageEmbed()
+				.setTitle('Adicionada à queue. ')
+				.setDescription(
+					`[${song.title}](${song.url}) pedida por: ${song.askedBy}`
+				)
+				.setColor('DARK_VIVID_PINK');
+			serverQueue.songs.push(song);
+			message.channel.send(msg);
+			return;
+		}
+	};
 
-        }
-
-        if(!serverQueue) {
-            const queueConstructor = {
-
-                txtChannel: message.channel,
-                vChannel: vc,
-                connection: null,
-                songs: [],
-                volume: 10,
-                playing: true,
-
-            };
-
-            queue.set(message.guild.id, queueConstructor);
-
-            queueConstructor.songs.push(song);
-
-            try{
-                let connection = await vc.join()
-                message.guild.me.voice.setSelfDeaf(true);
-                queueConstructor.connection = connection;
-                play(message.guild, queueConstructor.songs[0]);
-            }
-            catch(err){
-                console.error(err);
-                queue.delete(message.guild.id);
-                return message.channel.send(`Não foi possível conectar-me ao voice channel ${err}.`)
-            }
-        }else{
-                let msg = new Discord.MessageEmbed()
-                    .setTitle("Adicionada à queue. ")
-                    .setDescription(`[${song.title}](${song.url}) pedida por: ${song.askedBy}`)
-                    .setColor("DARK_VIVID_PINK")
-                serverQueue.songs.push(song);
-                message.channel.send(msg);
-                return;
-        }
-    }
-
-
-    if(url.match(/^https?:\/\/(www.youtube.com|youtube.com)\/playlist(.*)$/)){
-
-        try{
-            await ytpl(url).then(async playlist => {
-                message.channel.send(`A playlist: "${playlist.title}" foi adicionada.`)
-                playlist.items.forEach(async item => {
-                    await videoHandler(await ytdl.getInfo(item.shortUrl), message, vc, true);
-                })
-            })
-        }catch(err){
-            return message.channel.send(`Insere um link válido.\n${err}`)
-        }
-        m
-    }
-    else{
-
-        let result = await searcher.search(args.join(" "), {type: "video"});
-        if(result.first == null) return message.channel.send("Não foram encontrados resultados.");
-        //message.channel.send(result.first.url);
-        let songInfo = await ytdl.getInfo(result.first.url);
-        return videoHandler(songInfo, message, vc);
-
-    }
-
-    
+	if (url.match(/^https?:\/\/(www.youtube.com|youtube.com)\/playlist(.*)$/)) {
+		try {
+			await ytpl(url).then(async (playlist) => {
+				message.channel.send(`A playlist: "${playlist.title}" foi adicionada.`);
+				playlist.items.forEach(async (item) => {
+					await videoHandler(
+						await ytdl.getInfo(item.shortUrl),
+						message,
+						vc,
+						true
+					);
+				});
+			});
+		} catch (err) {
+			return message.channel.send(`Insere um link válido.\n${err}`);
+		}
+	} else {
+		let result = await searcher.search(args.join(' '), { type: 'video' });
+		if (result.first == null)
+			return message.channel.send('Não foram encontrados resultados.');
+		//message.channel.send(result.first.url);
+		let songInfo = await ytdl.getInfo(result.first.url);
+		return videoHandler(songInfo, message, vc);
+	}
 };
 
 module.exports.config = {
-
-    name: "play",
-    aliases: ["p", "pl"]
-
-}
+	name: 'play',
+	aliases: ['p', 'pl'],
+};
--- index.js
+++ index.js
@@ -1,85 +1,72 @@
-require('dotenv').config()
-const fs = require('fs')
-const Discord = require('discord.js')
+require('dotenv').config();
+const fs = require('fs');
+const Discord = require('discord.js');
 
-const {YTSearcher} = require('ytsearcher');
+const { YTSearcher } = require('ytsearcher');
 
 const searcher = new YTSearcher({
-    key: process.env.youtube_api,
-    revealed: true
+	key: process.env.youtube_api,
+	revealed: true,
 });
 
-const client = new Discord.Client()
+const client = new Discord.Client();
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
 
-fs.readdir('./commands/', (err,files) => {
-    if(err) console.log(err);
+fs.readdir('./commands/', (err, files) => {
+	if (err) console.log(err);
 
-    files.forEach( (file) => {
-        if(!file.endsWith(".js")) return;
-        let cmd = require(`./commands/${file}`);
-        let cmdName = cmd.config.name;
-        client.commands.set(cmdName, cmd);
-        cmd.config.aliases.forEach(alias => {
-            client.aliases.set(alias, cmdName)
-
-        });
-    })
+	files.forEach((file) => {
+		if (!file.endsWith('.js')) return;
+		let cmd = require(`./commands/${file}`);
+		let cmdName = cmd.config.name;
+		client.commands.set(cmdName, cmd);
+		cmd.config.aliases.forEach((alias) => {
+			client.aliases.set(alias, cmdName);
+		});
+	});
 });
 
 const queue = new Map();
 
-client.on("ready", ()  => {
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.tag}!`);
 
-    console.log(`Logged in as ${client.user.tag}!`);
-    
-    client.user.setActivity("!help", {
-
-    type: "WATCHING"
-
-    })
-
-})
-
-client.on("message", async (message) => {
-
-    const {prefix} = require('./json/config.json');
-
-    let prefixes = JSON.parse(fs.readFileSync("./json/prefixes.json", "utf-8"));
-
-    if(!prefixes[message.guild.id]){
-
-        prefixes[message.guild.id] = {
-
-            prefixes: prefix,
-
-        };
-
-    }
-
-    let prefixNew = prefixes[message.guild.id].prefixes;
-
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-
-    const command = args.shift().toLowerCase();
-
-    if(!message.content.startsWith(prefixNew) || message.author.bot) return;
-
-    const cmd = client.commands.get(command) || client.commands.get(client.aliases.get(command));
-
-    if(!cmd) return;
-    try{
-        cmd.run(client, message, args, queue, searcher, prefixNew);
-    }catch(err){
-        return console.error(err);
-    }
-
-
+	client.user.setActivity('!help', {
+		type: 'WATCHING',
+	});
 });
 
+client.on('message', async (message) => {
+	const { prefix } = require('./json/config.json');
 
+	let prefixes = JSON.parse(fs.readFileSync('./json/prefixes.json', 'utf-8'));
 
+	if (!prefixes[message.guild.id]) {
+		prefixes[message.guild.id] = {
+			prefixes: prefix,
+		};
+	}
 
+	let prefixNew = prefixes[message.guild.id].prefixes;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/g);
+
+	const command = args.shift().toLowerCase();
+
+	if (!message.content.startsWith(prefixNew) || message.author.bot) return;
+
+	const cmd =
+		client.commands.get(command) ||
+		client.commands.get(client.aliases.get(command));
+
+	if (!cmd) return;
+
+	try {
+		cmd.run(client, message, args, queue, searcher, prefixNew);
+	} catch (err) {
+		return console.error(err);
+	}
+});
 
 client.login(process.env.token);

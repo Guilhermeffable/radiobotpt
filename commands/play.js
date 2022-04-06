5a0d720cb79990cabe17ce83496787290ff72a33
guilherme.luis <guilherme.luis@janeladigital.com>
2022-04-06 15:29:46 +0100

    ongoing

--- commands/play.js
+++ commands/play.js
@@ -83,9 +83,8 @@ module.exports.run = async (client, message, args, queue, searcher) => {
 					channelId: message.member.voice.channel.id,
 					guildId: message.guild.id,
 					adapterCreator: message.guild.voiceAdapterCreator,
-				}).then((connection) => {
-					connection.voice.setSelfDeaf(true);
 				});
+				connection.voice.message.guild.me.setDeaf(true);
 				queueConstructor.connection = connection;
 				play(message.guild, queueConstructor.songs[0]);
 			} catch (err) {

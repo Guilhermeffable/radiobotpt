a37722d3ceeaaf16b513177ef18dcfad69158f80
guilherme.luis <guilherme.luis@janeladigital.com>
2022-04-06 15:23:37 +0100

    selfDeafen

--- commands/play.js
+++ commands/play.js
@@ -84,7 +84,7 @@ module.exports.run = async (client, message, args, queue, searcher) => {
 					guildId: message.guild.id,
 					adapterCreator: message.guild.voiceAdapterCreator,
 				});
-				message.guild.me.voice.setSelfDeaf(true);
+				connection.voice.setSelfDeaf(true);
 				queueConstructor.connection = connection;
 				play(message.guild, queueConstructor.songs[0]);
 			} catch (err) {

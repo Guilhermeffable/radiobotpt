d8ead1b40982aaf7fb7ff8ecbb9cb55a1fc83ccd
Guilherme Luís <guilhermeffable@gmail.com>
2022-04-06 20:32:15 +0100

    fixed all embeds - hopefuly

--- commands/prefix.js
+++ commands/prefix.js
@@ -4,61 +4,54 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports.run = (client, message, args, queue, searcher, prefix) => {
-
-
     let newPrefix = message.content.slice(prefix.length).trim().split(/ +/g);
 
     console.log();
 
-    let prefixes = JSON.parse(fs.readFileSync(path.join(__dirname, "../json/prefixes.json"), "utf-8"));
-    
+    let prefixes = JSON.parse(
+        fs.readFileSync(path.join(__dirname, '../json/prefixes.json'), 'utf-8')
+    );
 
     let msg = new Discord.MessageEmbed()
-            .setTitle("Prefixo")
-            .setColor("DARK_VIVID_PINK")
-            .setTimestamp(new Date())
-            .setFooter(message.guild.name, message.guild.iconURL())
-        
+        .setTitle('Prefixo')
+        .setColor('DARK_VIVID_PINK')
+        .setTimestamp(new Date())
+        .setFooter({
+            text: message.guild.name,
+            iconURL: message.guild.iconURL()
+        });
 
-    if(newPrefix[1] === undefined){
-        
-        msg.setDescription('Prefixo inválido, insere outro.')
-        msg.addField('Prefixo atual:',  '`' + `${prefix}` + '`' );
+    if (newPrefix[1] === undefined) {
+        msg.setDescription('Prefixo inválido, insere outro.');
+        msg.addField('Prefixo atual:', '`' + `${prefix}` + '`');
         message.channel.send(msg);
-
-    }
-    else{
-
-        if(newPrefix[1].match(/[\.!\/\\\/\-\?\$\%\&\=\*\+]/)){
-
-            msg.setDescription('Prefixo válido, o novo prefixo é ' + '`' + `${newPrefix[1]}` + '`');
+    } else {
+        if (newPrefix[1].match(/[\.!\/\\\/\-\?\$\%\&\=\*\+]/)) {
+            msg.setDescription(
+                'Prefixo válido, o novo prefixo é ' +
+                    '`' +
+                    `${newPrefix[1]}` +
+                    '`'
+            );
             prefixes[message.guild.id] = {
-
-                prefixes : newPrefix[1]
-
-            }
+                prefixes: newPrefix[1]
+            };
             message.channel.send(msg);
-            fs.writeFile(path.join(__dirname, "../json/prefixes.json"), JSON.stringify(prefixes), (err) => {
-
-                if(err) console.error(err);
-
-            })
-
-            
-    
-        }else{
-
-            msg.setDescription('Prefixo inválido, insere outro.')
-            message.channel.send(msg);
-
+            fs.writeFile(
+                path.join(__dirname, '../json/prefixes.json'),
+                JSON.stringify(prefixes),
+                (err) => {
+                    if (err) console.error(err);
+                }
+            );
+        } else {
+            msg.setDescription('Prefixo inválido, insere outro.');
+            message.channel.send({ embeds: msg });
         }
-
     }
-
-}
-
+};
 
 module.exports.config = {
-    name:"prefix",
+    name: 'prefix',
     aliases: []
-}
+};
--- commands/queue.js
+++ commands/queue.js
@@ -1,39 +1,37 @@
 const Discord = require('discord.js');
 
-
 module.exports.run = (client, message, args, queue, searcher) => {
-
     console.log(queue);
 
     const serverQueue = queue.get(message.guild.id);
 
     let embed = new Discord.MessageEmbed()
-        .setTitle("Queue de reprodução")
-        .setColor("DARK_VIVID_PINK")
+        .setTitle('Queue de reprodução')
+        .setColor('DARK_VIVID_PINK')
         .setTimestamp(new Date())
-        .setFooter(message.guild.name, message.guild.iconURL())
+        .setFooter({
+            text: message.guild.name,
+            iconURL: message.guild.iconURL()
+        })
         .setAuthor(client.user.username);
 
-    if(!serverQueue){
-        embed.addField("Não há nada para ser reproduzido.")
-
+    if (!serverQueue) {
+        embed.addField('Não há nada para ser reproduzido.');
+    } else {
+        serverQueue.songs.map((item, pos) => {
+            embed.addField(
+                `${pos + 1} - [${serverQueue.songs[0].title}](${
+                    serverQueue.songs[0].url
+                })`,
+                `@${item.askedByUsername}`
+            );
+        });
     }
-    else{
 
-        serverQueue.songs.map( (item, pos) => {
-
-            embed.addField(`${pos + 1} - [${serverQueue.songs[0].title}](${serverQueue.songs[0].url})`,  `@${item.askedByUsername}`)
-    
-    
-        })
-    }
-
-
-    message.channel.send(embed);
-
-}
+    message.channel.send({ embeds: msg });
+};
 
 module.exports.config = {
-    name: "queue",
-    aliases: ["q"]
-}
+    name: 'queue',
+    aliases: ['q']
+};
